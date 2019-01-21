b7f6919289154a10ae813a725d89eb1678e61bce
Gang125 <46886103+Gang125@users.noreply.github.com>
2019-01-21 10:59:41 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,7 +20,7 @@ const gif = require("gif-search");
 
 const client = new Discord.Client({disableEveryone: true});
 
-const prefix = "2";
+const prefix = "G";
 /////////////////////////
 ////////////////////////
 
@@ -349,7 +349,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : EX Clan ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`1play | Last Music`,"http://twitch.tv/Death Shop")
+client.user.setGame(`Gplay | Gang Server.`,"http://twitch.tv/Death Shop")
 client.user.setStatus("dnd")
 });
 
